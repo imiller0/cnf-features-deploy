8d15f4283c3eb572691aacea3ca8155590f82e6e
Ian Miller <imiller@redhat.com>
2021-08-16 17:34:11 -0400

    Modify container to generate day-2 policy CRs
    Signed-off-by: Ian Miller <imiller@redhat.com>

--- ztp/resource-generator/src/common.sh
+++ ztp/resource-generator/src/common.sh
@@ -5,4 +5,6 @@ export TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
 export CACERT=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
 export NAMESPACE=$(cat /var/run/secrets/kubernetes.io/serviceaccount/namespace)
 
+export configmap=pgt-rv
+
 oc login $APISERVER --token=$TOKEN --certificate-authority=$CACERT &> /dev/null
--- ztp/resource-generator/src/pre-sync-entrypoint.sh
+++ ztp/resource-generator/src/pre-sync-entrypoint.sh
@@ -3,20 +3,20 @@
 . $(dirname "$0")/common.sh
 
 # Delete old resource version configmap if present
-if oc get configmap/rv &> /dev/null; then
-    oc delete configmap/rv &> /dev/null
+if oc get configmap/${configmap} &> /dev/null; then
+    oc delete configmap/${configmap} &> /dev/null
 fi
 
-RESP=$(curl -s -w "%{http_code}" $APISERVER/apis/ran.openshift.io/v1/siteconfigs --header "Authorization: Bearer $TOKEN" --cacert $CACERT)
+RESP=$(curl -s -w "%{http_code}" $APISERVER/apis/ran.openshift.io/v1/policygentemplates --header "Authorization: Bearer $TOKEN" --cacert $CACERT)
 RC=$(echo $RESP | python -c "print(input()[-3:])")
 RV=$(echo $RESP | python -c "print(input()[:-3])" | jq -rM '.metadata.resourceVersion')
 
 if [ $RC != "200" ];then
-    echo "ztp-site-generator.presync $(date -R) ERROR [pre-sync-entrypoint] $APISERVER/apis/ran.openshift.io/v1/siteconfigs call returned $RC" >> /proc/1/fd/1
+    echo "ztp-site-generator.presync $(date -R) ERROR [pre-sync-entrypoint] $APISERVER/apis/ran.openshift.io/v1/policygentemplates call returned $RC" >> /proc/1/fd/1
     exit 1
 else
     # Store in configmap
-    if oc create configmap rv --from-literal=sitesResourceVersion=$RV; then
+    if oc create configmap ${configmap} --from-literal=pgtResourceVersion=$RV; then
         # Log even if ran manually during debugging
         echo "ztp-site-generator.presync $(date -R) INFO [pre-sync-entrypoint] Recording RAN sites resourceVersion = $RV" >> /proc/1/fd/1
     else
